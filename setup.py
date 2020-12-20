76fc2963699ffeff15cd293b842483e52f84f799
jiaying <280824854@qq.com>
2020-12-20 14:28:52 +0800

    release 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple_graph",
-    version="0.1.4",
+    version="0.1.5",
     author="Jiaying Wang",
     author_email="jiaying@sjzu.edu.cn",
     description="A simple graph package",
