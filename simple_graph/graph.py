16d5fdf93a568d469b925199b5c0534c5e0f39ce
jiaying <280824854@qq.com>
2021-01-30 15:48:23 +0800

    add random graph generator

--- simple_graph/graph.py
+++ simple_graph/graph.py
@@ -1,4 +1,5 @@
 from math import inf
+import random
 from collections import defaultdict
 from .vertices import Vertices
 from .edges import Edges
@@ -226,6 +227,14 @@ class Graph:
   
   def total_vertex_weight(self):
     return sum([self.vertex(v).weight if hasattr(self.vertex(v), 'weight') else 1.0 for v in self.vertices])
+
+  def generate_random_graph(self, n, p):
+    for i in range(n):
+      self.add_vertex(i+1)
+    for i in self.vertices:
+      for j in self.vertices:
+        if (random.random() < p):
+          self.add_edge(i, j)
 
   def find_isolated_vertices(self):
     isolated = []
